dac0a6094db91da2178a9b184ff2af56cad2ae06
Jaaz4hop <franccyril31@gmail.com>
2026-09-12 13:16:36 +0200

    Add main.js + edit css and html

---
+++ main.js
@@ -0,0 +1,19 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    'x-api-key': 'mAA8fg86dGMwAMZOU1T5f6PAKI8WfIEpvo2iqewC'
+  }
+};
+
+async function chargerDonnees() {
+    try {
+        const reponse = await fetch('https://api.sportradar.com/rugby-league/trial/v3/en/competitions/sr%3Acompetition%3A302/info.json', options)
+        const donnees = await reponse.json();
+        console.log(donnees);
+    }
+    catch (erreur){
+        console.log(`Une erreur est survenue : + ${erreur}`);
+    }
+}
+
